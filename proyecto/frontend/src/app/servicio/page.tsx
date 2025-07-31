db8b85b8d4bed4dc33fdc8ec6e58373463cc0ce4
Flor Gomez <up220485@alumnos.upa.edu.mx>
2025-07-31 12:24:43 -0600

    imágenes y unos ligeros acomodos

--- proyecto/frontend/src/app/servicio/page.tsx
+++ proyecto/frontend/src/app/servicio/page.tsx
@@ -9,7 +9,7 @@ const servicios = [
         nombre: 'Masaje Relajante',
         descripcion: 'Masaje para aliviar el estrés y relajar los músculos.',
         precio: 500,
-        imagen: '/images/masaje.jpg'
+        imagen: '/images/masajerelajante.jpg'
     },
     {
         id: 2,
@@ -23,8 +23,36 @@ const servicios = [
         nombre: 'Spa de Manos',
         descripcion: 'Cuidado y embellecimiento de manos y uñas.',
         precio: 250,
-        imagen: '/images/manos.jpg'
+        imagen: '/images/spamanos.jpg'
     },
+    {
+        id: 4,
+        nombre: 'Spa de Pies',
+        descripcion: 'Relajación y cuidado especial para tus pies.',
+        precio: 300,
+        imagen: '/images/spapies.jpg'
+    },
+    {
+    id: 5,
+    nombre: 'Limpieza Facial Profunda',
+    descripcion: 'Tratamiento para limpiar y purificar la piel del rostro a profundidad.',
+    precio: 400,
+    imagen: '/images/limpfacialprof.jpg'
+},
+{
+    id: 6,
+    nombre: 'Depilación con Cera',
+    descripcion: 'Eliminación de vello corporal y facial con cera profesional.',
+    precio: 300,
+    imagen: '/images/depilación.jpg' 
+},
+{
+    id: 7,
+    nombre: 'Exfoliación Corporal',
+    descripcion: 'Eliminación de células muertas para una piel suave y renovada.',
+    precio: 350,
+    imagen: '/images/exfcorporal.jpg'
+},
     // Agrega más servicios según lo que ofrezca tu spa
 ];
 
